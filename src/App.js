8e494af3c26ca44e66fa2ba121d32c5ae8779262
SamSmith21 <sammy_smith_@hotmail.com>
2022-02-13 10:23:35 +0000

    Input search box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header">Sam's Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Samantha Smith</footer>
+      </div>
     </div>
   );
 }
